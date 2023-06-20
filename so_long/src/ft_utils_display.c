3e99d692c687e2984dbfd7c89e20d0263bc2ec33
Alexandre Herrmann <aherrman@k0r3p4.42mulhouse.fr>
2023-06-20 09:04:28 +0200

    ok

--- so_long/src/ft_utils_display.c
+++ so_long/src/ft_utils_display.c
@@ -6,7 +6,7 @@
 /*   By: aherrman <aherrman@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 09:48:41 by aherrman          #+#    #+#             */
-/*   Updated: 2023/06/19 11:30:05 by aherrman         ###   ########.fr       */
+/*   Updated: 2023/06/20 08:57:05 by aherrman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,10 +51,26 @@ int	ft_displayp(t_mlx *pic, mlx_t *mlx, t_temp temp, t_game *game)
 
 int	ft_display1(t_mlx *pic, mlx_t *mlx, t_temp temp, t_game *game)
 {
-	if (ft_displayf(pic, mlx, game, temp) != 0)
-		return (1);
-	if (mlx_image_to_window(mlx, pic->wall, temp.i.t1 * 32, temp.i.t0 * 32) < 0)
-		return (1);
+	int	i;
+
+	if (game->msize->x - temp.i.t1 * temp.i.t0 > 3)
+		i = game->msize->x - game->msize->y + temp.i.t1 - temp.i.t0
+			- game->step;
+	else
+		i = game->msize->x + game->msize->y - temp.i.t1 + temp.i.t0
+			+ game->step;
+	if (i % 3 == 0)
+	{
+		if (mlx_image_to_window(mlx, pic->wall, temp.i.t1 * 32, temp.i.t0
+				* 32) < 0)
+			return (1);
+	}
+	else
+	{
+		if (mlx_image_to_window(mlx, pic->wall2, temp.i.t1 * 32, temp.i.t0
+				* 32) < 0)
+			return (1);
+	}
 	return (0);
 }
 
